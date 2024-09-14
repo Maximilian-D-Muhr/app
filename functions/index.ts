d85a6de3278923b2363107f0d0721ed5d734fbea
Maximilian-D-Muhr <maximilian.muhr@me.com>
2024-09-15 00:29:14 +0200

    Update index.ts

--- functions/index.ts
+++ functions/index.ts
@@ -1,14 +1,39 @@
-import firebase from 'firebase-admin';
-import 'firebase-functions'; // Import to initialize FIREBASE_CONFIG 🙄
+import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
 
+// Parse Firebase config
 const config = JSON.parse(process.env.FIREBASE_CONFIG!);
-// tslint:disable-next-line:no-var-requires
-const serviceAccount = require('./service-account.json');
 
-firebase.initializeApp({
+// Import service account
+// tslint:disable-next-line:no-var-requires
+const serviceAccount = require('../service-account.json');
+
+// Initialize Firebase app
+admin.initializeApp({
     ...config,
-    credential: firebase.credential.cert(serviceAccount),
+    credential: admin.credential.cert(serviceAccount),
 });
 
+// Export existing functions
 export * from './lib/spotify-auth';
 export * from './lib/vote-processor';
+
+// Add new functions
+export const helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Musik DJ!");
+});
+
+export const createParty = functions.https.onCall((data, context) => {
+  // Implement logic to create a new party
+  const partyId = Math.random().toString(36).substring(2, 15);
+  return { partyId: partyId };
+});
+
+export const addSongToPlaylist = functions.https.onCall((data, context) => {
+  // Implement logic to add a song to the playlist
+  const { partyId, songId } = data;
+  // Here you would add the song to the playlist in the database
+  return { success: true, message: `Song ${songId} added to party ${partyId}` };
+});
+
+// Add more functions as needed for your Musik DJ project
